8278fb17bb86dd3558e1864bea6dd809cd42b8bb
Jose Enser <daroenser@gmail.com>
2021-11-01 15:08:45 -0300

    Adding EFCore

---
+++ RapidPay.Data/Models/CreditCard.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace RapidPay.Data.Models
+{
+    public partial class CreditCard
+    {
+        public string CardNumber { get; set; }
+        public string CardName { get; set; }
+        public int UserId { get; set; }
+        public short ExpirationMonth { get; set; }
+        public short ExpirationYear { get; set; }
+        public short VerficationCode { get; set; }
+    }
+}
---
+++ RapidPay.Data/Models/RapidPayContext.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+#nullable disable
+
+namespace RapidPay.Data.Models
+{
+    public partial class RapidPayContext : DbContext
+    {
+        public RapidPayContext()
+        {
+        }
+
+        public RapidPayContext(DbContextOptions<RapidPayContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<CreditCard> CreditCards { get; set; }
+        public virtual DbSet<User> Users { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Server=.;Database=RapidPay;Trusted_Connection=True;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("Relational:Collation", "SQL_Latin1_General_CP1_CI_AS");
+
+            modelBuilder.Entity<CreditCard>(entity =>
+            {
+                entity.HasKey(e => e.CardNumber);
+
+                entity.Property(e => e.CardNumber)
+                    .HasMaxLength(15)
+                    .IsUnicode(false)
+                    .IsFixedLength(true);
+
+                entity.Property(e => e.CardName)
+                    .IsRequired()
+                    .HasMaxLength(100)
+                    .IsUnicode(false);
+            });
+
+            modelBuilder.Entity<User>(entity =>
+            {
+                entity.Property(e => e.Id)
+                    .ValueGeneratedNever()
+                    .HasColumnName("ID");
+
+                entity.Property(e => e.Email)
+                    .IsRequired()
+                    .HasMaxLength(100);
+
+                entity.Property(e => e.FirstName)
+                    .IsRequired()
+                    .HasMaxLength(50);
+
+                entity.Property(e => e.LastName)
+                    .IsRequired()
+                    .HasMaxLength(50);
+
+                entity.Property(e => e.Password)
+                    .IsRequired()
+                    .HasMaxLength(100);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
